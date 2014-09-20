01f8ed2e8b8426a308f3282a7162b49c5581f256
tavisca-alimaye <alimaye@tavisca.com>
2014-09-20 19:52:49 +0530

    Initial Commit

---
+++ Database1/Database1/Table1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NOT NULL
+)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-ClientSupplierDocApp
+ClientSupplierMapper
 ====================
